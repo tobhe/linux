c5b4b2f2cd275f8d6ed476b31422ec9a6c777bd9
Hao Yao <hao.yao@intel.com>
2023-10-26 00:06:42 +0800

    UBUNTU: SAUCE: platform/x86: int3472: Add handshake GPIO function
    BugLink: https://bugs.launchpad.net/bugs/2031412
    
    Handshake pin is used for Lattice MIPI aggregator to enable the
    camera sensor. After pulled up, recommend to wail ~250ms to get
    everything ready.
    
    Signed-off-by: Hao Yao <hao.yao@intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    (cherry-picked from https://lore.kernel.org/all/20231007021225.9240-1-hao.yao@intel.com)
    Signed-off-by: You-Sheng Yang <vicamo.yang@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Roxana Nicolescu <roxana.nicolescu@canonical.com>
    Signed-off-by: Roxana Nicolescu <roxana.nicolescu@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- drivers/platform/x86/intel/int3472/discrete.c
+++ drivers/platform/x86/intel/int3472/discrete.c
@@ -190,6 +190,10 @@ static void int3472_get_con_id_and_polarity(struct acpi_device *adev, u8 *type,
 		*con_id = "power-enable";
 		*gpio_flags = GPIO_ACTIVE_HIGH;
 		break;
+	case INT3472_GPIO_TYPE_HANDSHAKE:
+		*con_id = "handshake";
+		*gpio_flags = GPIO_ACTIVE_HIGH;
+		break;
 	default:
 		*con_id = "unknown";
 		*gpio_flags = GPIO_ACTIVE_HIGH;
@@ -281,6 +285,8 @@ static int skl_int3472_handle_gpio_resources(struct acpi_resource *ares,
 	switch (type) {
 	case INT3472_GPIO_TYPE_RESET:
 	case INT3472_GPIO_TYPE_POWERDOWN:
+		ret = skl_int3472_map_gpio_to_sensor(int3472, agpio, con_id, gpio_flags);
+	case INT3472_GPIO_TYPE_HANDSHAKE:
 		ret = skl_int3472_map_gpio_to_sensor(int3472, agpio, con_id, gpio_flags);
 		if (ret)
 			err_msg = "Failed to map GPIO pin to sensor\n";
