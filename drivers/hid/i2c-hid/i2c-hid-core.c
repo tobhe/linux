530db23eeb48cd39327a582733474f449d4e9cd9
Aleksandrs Vinarskis <alex.vinarskis@gmail.com>
2024-09-25 12:01:59 +0200

    HID: i2c-hid: introduce re-power-on quirk
    It appears some keyboards from vendor 'QTEC' will not work properly until
    suspend & resume.
    
    Empirically narrowed down to solution of re-sending power on command
    _after_ initialization was completed before the end of initial probing.
    
    Signed-off-by: Aleksandrs Vinarskis <alex.vinarskis@gmail.com>

--- drivers/hid/i2c-hid/i2c-hid-core.c
+++ drivers/hid/i2c-hid/i2c-hid-core.c
@@ -50,6 +50,7 @@
 #define I2C_HID_QUIRK_BAD_INPUT_SIZE		BIT(3)
 #define I2C_HID_QUIRK_NO_WAKEUP_AFTER_RESET	BIT(4)
 #define I2C_HID_QUIRK_NO_SLEEP_ON_SUSPEND	BIT(5)
+#define I2C_HID_QUIRK_RE_POWER_ON		BIT(6)
 
 /* Command opcodes */
 #define I2C_HID_OPCODE_RESET			0x01
@@ -1048,7 +1049,11 @@ static int i2c_hid_core_register_hid(struct i2c_hid *ihid)
 		return ret;
 	}
 
-	return 0;
+	/* At least some QTEC devices need this after initialization */
+	if (ihid->quirks & I2C_HID_QUIRK_RE_POWER_ON)
+		ret = i2c_hid_set_power(ihid, I2C_HID_PWR_ON);
+
+	return ret;
 }
 
 static int i2c_hid_core_probe_panel_follower(struct i2c_hid *ihid)
