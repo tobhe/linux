290ca5484b2191c94476b27000900e5c678845aa
Dikshita Agarwal <quic_dikshita@quicinc.com>
2024-10-14 14:37:28 +0530

    media: iris: introduce host firmware interface with necessary hooks
    Host firmware interface (HFI) is well defined set of interfaces
    for communication between host driver and firmware.
    The commands and responses are exchanged in form of packets.
    One or multiple packets are grouped under packet header.
    Each packet has packet type which describes the specific HFI
    and payload which holds the corresponding value for that HFI.
    
    Signed-off-by: Dikshita Agarwal <quic_dikshita@quicinc.com>

--- drivers/media/platform/qcom/iris/iris_hfi_gen1.h
+++ drivers/media/platform/qcom/iris/iris_hfi_gen1.h
@@ -6,8 +6,11 @@
 #ifndef _IRIS_HFI_GEN1_H_
 #define _IRIS_HFI_GEN1_H_
 
+struct iris_core;
 struct iris_inst;
 
+void iris_hfi_gen1_command_ops_init(struct iris_core *core);
+void iris_hfi_gen1_response_ops_init(struct iris_core *core);
 struct iris_inst *iris_hfi_gen1_get_instance(void);
 
 #endif
