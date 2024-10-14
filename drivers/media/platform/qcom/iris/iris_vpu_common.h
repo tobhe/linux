ba207f73cc1969ee07d8591cec8e39f89beb4e59
Dikshita Agarwal <quic_dikshita@quicinc.com>
2024-10-14 14:37:27 +0530

    media: iris: implement boot sequence of the firmware
    Set memory region to firmware and implement boot sequence.
    
    Signed-off-by: Dikshita Agarwal <quic_dikshita@quicinc.com>

---
+++ drivers/media/platform/qcom/iris/iris_vpu_common.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef _IRIS_VPU_COMMON_H_
+#define _IRIS_VPU_COMMON_H_
+
+struct iris_core;
+
+int iris_vpu_boot_firmware(struct iris_core *core);
+
+#endif
