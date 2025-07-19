3ebdb0766488d5a923ef4580bfb863d4f3dfd819
Tobias Heider <tobias.heider@canonical.com>
2025-07-20 00:48:13 +0200

    UBUNTU: install compressed vmlinuz.efi (LP: #2098111)
    Signed-off-by: Tobias Heider <tobias.heider@canonical.com>

--- debian.qcom-x1e/rules.d/arm64.mk
+++ debian.qcom-x1e/rules.d/arm64.mk
@@ -1,8 +1,8 @@
 build_arch	= arm64
 defconfig	= defconfig
 flavours	= qcom-x1e
-build_image	= Image.gz
-kernel_file	= arch/$(build_arch)/boot/Image.gz
+build_image	= vmlinuz.efi
+kernel_file	= arch/$(build_arch)/boot/vmlinuz.efi
 install_file	= vmlinuz
 no_dumpfile	= true
 
