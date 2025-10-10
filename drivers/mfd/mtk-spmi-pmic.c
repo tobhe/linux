ea931d097b5c9ca195d7ac4ea68aa519aec67d41
Nicolas Frattaroli <nicolas.frattaroli@collabora.com>
2025-10-10 20:41:40 +0200

    squash! drivers: mfd: Add support for MediaTek SPMI PMICs and MT6363/73
    mtk_spmi_pmic_irq_xlate gets called from irq_domain_translate, which has
    an fwspec and disassembles it into intsize, device_node and intspec.
    
    mtk_spmi_pmic_irq_xlate then reassembles it into an fwspec, just to then
    check param_count, which is literally what intsize is.
    
    Just check for intsize instead, and replace the WARN_ON with a proper
    dev_err while at it.
    
    Signed-off-by: Nicolas Frattaroli <nicolas.frattaroli@collabora.com>

--- drivers/mfd/mtk-spmi-pmic.c
+++ drivers/mfd/mtk-spmi-pmic.c
@@ -154,11 +154,11 @@ static int mtk_spmi_pmic_irq_xlate(struct irq_domain *d, struct device_node *ctr
 {
 	struct mtk_spmi_pmic *pmic = d->host_data;
 	struct device *dev = pmic->dev;
-	struct irq_fwspec fwspec;
 
-	of_phandle_args_to_fwspec(ctrlr, intspec, intsize, &fwspec);
-	if (WARN_ON(fwspec.param_count < 3))
+	if (intsize < 3) {
+		dev_err(dev, "Expected IRQ specifier of size 3, got %u\n", intsize);
 		return -EINVAL;
+	}
 
 	/*
 	 * The IRQ number in intspec[0] is ignored on purpose here!
