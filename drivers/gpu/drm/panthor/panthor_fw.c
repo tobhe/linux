333e4918d8fd49e15be732203500f087ea6f2b24
Jianfeng Liu <liujianfeng1994@gmail.com>
2025-11-06 16:46:22 +0800

    drm/panthor: use firmware path from mainline kernel

--- drivers/gpu/drm/panthor/panthor_fw.c
+++ drivers/gpu/drm/panthor/panthor_fw.c
@@ -752,8 +752,13 @@ static int panthor_fw_load(struct panthor_device *ptdev)
 	const struct firmware *fw = NULL;
 	struct panthor_fw_binary_iter iter = {};
 	struct panthor_fw_binary_hdr hdr;
-	char fw_path[20] = "mali_csffw.bin";
+	char fw_path[128];
 	int ret;
+
+	snprintf(fw_path, sizeof(fw_path), "arm/mali/arch%d.%d/%s",
+		 (u32)GPU_ARCH_MAJOR(ptdev->gpu_info.gpu_id),
+		 (u32)GPU_ARCH_MINOR(ptdev->gpu_info.gpu_id),
+		 CSF_FW_NAME);
 
 	ret = request_firmware(&fw, fw_path, ptdev->base.dev);
 	if (ret) {
@@ -1422,3 +1427,4 @@ err_unplug_fw:
 }
 
 MODULE_FIRMWARE("arm/mali/arch10.8/mali_csffw.bin");
+MODULE_FIRMWARE("arm/mali/arch12.8/mali_csffw.bin");
