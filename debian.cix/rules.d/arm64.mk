1baa214a5d74d27cb7e6162fab5a947dd52c2b31
Tobias Heider <me@tobhe.de>
2026-02-18 15:33:53 +0100

    packaging updates

--- debian.cix/rules.d/arm64.mk
+++ debian.cix/rules.d/arm64.mk
@@ -1,8 +1,8 @@
 build_arch	= arm64
 defconfig	= defconfig
 flavours	= cix
-build_image	= Image.gz
-kernel_file	= arch/$(build_arch)/boot/Image.gz
+build_image	= vmlinuz.efi
+kernel_file	= arch/$(build_arch)/boot/vmlinuz.efi
 install_file	= vmlinuz
 no_dumpfile	= true
 uefi_signed     = true
