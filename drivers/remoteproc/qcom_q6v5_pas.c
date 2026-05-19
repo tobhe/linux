2b363879c6c17eb5395b79813bb8169c79a24e2a
Jingyi Wang <jingyi.wang@oss.qualcomm.com>
2026-05-19 00:24:24 -0700

    remoteproc: qcom_q6v5_pas: Add SoCCP node on Kaanapali
    The SoC Control Processor (SoCCP) is small RISC-V MCU that controls
    USB Type-C, battery charging and various other functions on Qualcomm SoCs.
    It provides a solution for control-plane processing, reducing per-subsystem
    microcontroller reinvention. Add support for SoCCP PAS loader on Kaanapali
    platform.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Reviewed-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
    Signed-off-by: Jingyi Wang <jingyi.wang@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260519-knp-soccp-v6-6-cf5d0e194b5f@oss.qualcomm.com
    Signed-off-by: Abel Vesa <abel.vesa@oss.qualcomm.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -1630,8 +1630,26 @@ static const struct qcom_pas_data sm8750_mpss_resource = {
 	.region_assign_vmid = QCOM_SCM_VMID_MSS_MSA,
 };
 
+static const struct qcom_pas_data kaanapali_soccp_resource = {
+	.crash_reason_smem = 656,
+	.firmware_name = "soccp.mbn",
+	.dtb_firmware_name = "soccp_dtb.mbn",
+	.pas_id = 51,
+	.dtb_pas_id = 0x41,
+	.proxy_pd_names = (char*[]){
+		"cx",
+		"mx",
+		NULL
+	},
+	.ssr_name = "soccp",
+	.sysmon_name = "soccp",
+	.auto_boot = true,
+	.early_boot = true,
+};
+
 static const struct of_device_id qcom_pas_of_match[] = {
 	{ .compatible = "qcom,eliza-adsp-pas", .data = &sm8550_adsp_resource },
+	{ .compatible = "qcom,kaanapali-soccp-pas", .data = &kaanapali_soccp_resource },
 	{ .compatible = "qcom,milos-adsp-pas", .data = &sm8550_adsp_resource },
 	{ .compatible = "qcom,milos-cdsp-pas", .data = &milos_cdsp_resource },
 	{ .compatible = "qcom,milos-mpss-pas", .data = &sm8450_mpss_resource },
