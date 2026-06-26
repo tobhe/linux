507d4f1974b2057ea31341b2d9f9c7401c8d5916
Jason Pettit <jason.pettit@oss.qualcomm.com>
2026-06-25 22:47:22 -0700

    firmware: qcom: scm: Allow QSEECOM on HP EliteBook X G2q 14 AI
    The HP EliteBook X G2q 14" Next Gen AI PC is a Snapdragon X2 Elite
    (Glymur) laptop whose firmware exposes EFI variables through QSEECOM.
    Add its board compatible to the QSEECOM allowlist so that EFI variable
    access (efivarfs) works on this machine.
    
    Without it the kernel reports "qseecom: untested machine, skipping" and
    efivarfs is empty.
    
    Signed-off-by: Jason Pettit <jason.pettit@oss.qualcomm.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260625-glymur-send-v2-3-00905324ffbf@oss.qualcomm.com
    Signed-off-by: Abel Vesa <abel.vesa@oss.qualcomm.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -2296,6 +2296,7 @@ static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "dell,xps13-9345" },
 	{ .compatible = "ecs,liva-qc710" },
 	{ .compatible = "hp,elitebook-ultra-g1q" },
+	{ .compatible = "hp,elitebook-x-g2q" },
 	{ .compatible = "hp,omnibook-x14" },
 	{ .compatible = "huawei,gaokun3" },
 	{ .compatible = "lenovo,flex-5g" },
